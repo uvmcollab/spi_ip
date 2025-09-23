9b7fd35cc4376a51d5f3ef1ff42ed5308c9f43d6
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2025-09-23 19:57:28 +0200

    fix: top_coverage.sv disable cov

--- verification/uvm/env/top_coverage.sv
+++ verification/uvm/env/top_coverage.sv
@@ -25,7 +25,7 @@ endclass : top_coverage
 function top_coverage::new(string name, uvm_component parent);
   super.new(name, parent);
   m_trans = spi_uvc_sequence_item::type_id::create("m_trans");
-  m_cov   = new();
+  //m_cov   = new();
 endfunction : new
 
 
@@ -35,12 +35,12 @@ endfunction : build_phase
 
 function void top_coverage::write_spi(input spi_uvc_sequence_item t);
   m_trans = t;
-  m_cov.sample();
+  //m_cov.sample();
 endfunction : write_spi
 
 
 function void top_coverage::report_phase(uvm_phase phase);
-  `uvm_info(get_type_name(), $sformatf("FINAL Coverage Score = %3.1f%%", m_cov.get_coverage()), UVM_DEBUG)
+  //`uvm_info(get_type_name(), $sformatf("FINAL Coverage Score = %3.1f%%", m_cov.get_coverage()), UVM_DEBUG)
 endfunction : report_phase
 
 `endif  // TOP_SCOREBOARD_SV
